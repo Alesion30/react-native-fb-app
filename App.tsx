c02202af474dcf02cbf2a28139e11856a998b813
Yuta Ohira <alesion90@gmail.com>
2022-09-11 00:19:03 +0900

    ユーザー追加機能を実装

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 import { userApi } from './src/api/user';
 import { User } from './src/domains/user';
 
 export default function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [name, onChangeName] = useState('');
+  const [age, onChangeAge] = useState('');
 
   useEffect(() => {
     // DBからユーザー一覧を取得
@@ -14,13 +16,43 @@ export default function App() {
     });
   }, []);
 
+  // ユーザーをDBに登録
+  const createUser = async () => {
+    if (name === '') Alert.alert('名前を入力してください')
+    const _age = age ? parseInt(age) : undefined;
+    userApi.createUser(name, _age);
+
+    onChangeName('');
+    onChangeAge('');
+  }
+
   return (
     <View style={styles.container}>
       <View>
         <Text>ユーザー一覧</Text>
         {users.map((user, i) => {
-          return <Text>{i}: {user.name}</Text>;
+          return (
+            <Text>
+              {i}: {user.name} {user.age ?? '??'}歳
+            </Text>
+          );
         })}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeName}
+            value={name}
+            placeholder="名前を入力"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeAge}
+            value={age}
+            keyboardType='numeric'
+            placeholder="年齢を入力"
+          />
+          <Button title="追加する" onPress={createUser} />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -33,5 +65,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    marginVertical: 20,
+  },
+  input: {
+    height: 40,
+    width: 250,
+    marginVertical: 5,
+    borderWidth: 1,
+    padding: 10,
   },
 });
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,15 +1,18 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { User } from '../domains/user';
 import { db } from '../services/firebase';
 
 const userCollectionRef = collection(db, 'users');
 
 type UserApi = {
+  /** ユーザー一覧を取得 */
   getUsers: () => Promise<User[]>;
+
+  /** ユーザーを新規作成 */
+  createUser: (name: string, age?: number) => Promise<void>;
 };
 
 export const userApi: UserApi = {
-  // ユーザー一覧を取得
   getUsers: async () => {
     const snap = await getDocs(userCollectionRef);
     const users: User[] = snap.docs.map((doc) => {
@@ -21,5 +24,11 @@ export const userApi: UserApi = {
       };
     });
     return users;
+  },
+  createUser: async (name, age) => {
+    await addDoc(userCollectionRef, {
+      name,
+      age,
+    });
   },
 };
